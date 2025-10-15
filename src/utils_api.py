4b07b3d16eb72b541ead56dfc98a13e640c99b65
Andrei Makarov <aomak@yandex.ru>
2025-10-15 16:24:25 +0300

    реализованы функции работы с API

---
+++ src/utils_api.py
@@ -0,0 +1,50 @@
+import requests
+
+def get_id(company_name):
+    """Получаем ID компании. Так как их в выдаче много, то берем с максиммальным
+    количеством вакансий"""
+    headers = {"User-Agent": "demo-script/1.0"}
+    resp = requests.get("https://api.hh.ru/employers",
+                        params={"text": company_name, "only_with_vacancies": "true", "per_page": 50},
+                        headers=headers)
+    if resp.status_code != 200:
+        raise Exception(f"GitHub API error: {resp.status_code}")
+    data = resp.json()["items"]
+
+    # Находим работодателя с наибольшим количеством вакансий
+    best = max(data, key=lambda x: x["open_vacancies"])
+    print("Основной работодатель:", best["name"])
+    print("ID:", best["id"])
+    print("Вакансий:", best["open_vacancies"])
+    print("URL:", best["alternate_url"])
+    print(best)
+    return best["id"]
+
+def get_vacancies_per_id(id):
+    """Формируем список вакансий компании на основе ID"""
+    headers = {"User-Agent": "demo-script/1.0"}
+    url = f"https://api.hh.ru/vacancies?employer_id={id}"
+    resp = requests.get(url, headers=headers)
+    if resp.status_code != 200:
+        raise Exception(f"GitHub API error: {resp.status_code}")
+    data = resp.json()["items"]
+    vacancy_list = []
+    for vacancy in data:
+        if not vacancy["salary"]:
+            salary = 0
+        else:
+            salary = vacancy.get("salary").get("from")
+        vacancy_dict = {
+            "name": vacancy.get("name"),
+            "city": vacancy.get("area").get("name"),
+            "salary": salary,
+            "link": vacancy.get("alternate_url")
+        }
+        vacancy_list.append(vacancy_dict)
+    return vacancy_list
+
+
+
+if __name__ == "__main__":
+    print(get_id("МТС"))
+    print(get_vacancies_per_id(2537115)[0])
